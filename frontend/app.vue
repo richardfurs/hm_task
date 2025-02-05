dca6a81286d77c798e21c1ad25119145ef9ab339
Richard <ricards.furs@gmail.com>
2025-02-05 18:09:16 +0200

    initial commit with docker setup and test api call

---
+++ frontend/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const message = ref('')
+
+onMounted(async () => {
+  const response = await fetch(`${useRuntimeConfig().public.apiBase}/test`)
+  message.value = await response.json()
+})
+</script>
